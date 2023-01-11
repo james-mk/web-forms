8a24103ddf0caaf085b5de97a0a663717bd0cc68
James MK <dev.jmkimani@gmail.com>
2023-01-11 16:31:27 +0300

    submit

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email</label>
     <input type="email" v-model="email" required />
     <label>Password</label>
     <input type="password" v-model="password" required />
+    <div v-if="passwordError" class="error">
+      {{ passwordError }}
+    </div>
     <label>Role</label>
     <select v-model="role" required>
       <option value="developer">Web Developer</option>
@@ -34,6 +37,10 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
+
+    <div class="submit">
+      <button>Create Account</button>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -54,6 +61,7 @@ export default {
       skills: [],
       otherSkillsArray: [],
       otherSkills: "",
+      passwordError: "",
     };
   },
   methods: {
@@ -69,6 +77,13 @@ export default {
       this.otherSkillsArray = this.otherSkillsArray.filter((item) => {
         return otherskill !== item;
       });
+    },
+
+    handleSubmit() {
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 characters";
     },
   },
 };
@@ -120,5 +135,22 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: #fff;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
